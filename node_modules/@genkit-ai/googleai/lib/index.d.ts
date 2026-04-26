a0f72aac9118f3e72a5aef3bb3a6c8c85e9eaed5
sanketh-kulkarni <ssskgk431@gmail.com>
2026-04-26 18:32:31 +0530

    initial commit

---
+++ node_modules/@genkit-ai/googleai/lib/index.d.ts
@@ -0,0 +1,6 @@
+import 'genkit';
+import 'genkit/plugin';
+export { P as PluginOptions, g as default, g as googleAI, t as textEmbedding004, a as textEmbeddingGecko001 } from './embedder-OOpL6C2r.js';
+export { GeminiConfig, GeminiVersionString, gemini, gemini10Pro, gemini15Flash, gemini15Flash8b, gemini15Pro, gemini20Flash, gemini20FlashExp, gemini20FlashLite, gemini20ProExp0205, gemini25FlashPreview0417, gemini25ProExp0325, gemini25ProPreview0325 } from './gemini.js';
+import '@google/generative-ai';
+import 'genkit/model';
